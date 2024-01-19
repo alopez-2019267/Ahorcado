3bfdc482b47f2946a503f59fffa3b8475859e55e
alopez-2019267 <alopez-2019267@kinal.edu.gt>
2024-01-19 08:31:39 -0600

    Tercer commit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function draw( ){
-    var canvas = document.getElementById("canvas");
-    if(canvas.getContext){
-        var ctx = canvas.getContext("2d");
-
-    }
-
-}
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d"); // lo pone en un plano 2d que puedo empezar a dibujar
+ctx.lineWidth = 8; // cuanto de ancho tiene la linea
+var startx= 480; // la posicion inicial de x 
+var starty = 200; // la posicion inicial de y
+ctx.strokeStyle = 'black';
+ctx.beginPath();
+ctx.arc(startx, starty, 20, 0, 2 * Math.PI); // La cabeza
+ctx.stroke();
